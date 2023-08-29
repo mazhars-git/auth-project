68a3f0d85fd085e1650979b7eacd903e43042881
mazhar <mazharulislam611@gmail.com>
2023-08-29 23:43:23 +0600

    menu added

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainMenu from '../Pages/MainMenu/MainMenu';
+
+const Main = () => {
+    return (
+        <div>
+            <MainMenu></MainMenu>
+        </div>
+    );
+};
+
+export default Main;
