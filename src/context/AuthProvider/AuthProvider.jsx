9410cbf47477d0246265c4cbfb374e9afdf62a67
mazhar <mazharulislam611@gmail.com>
2023-09-03 20:00:44 +0600

    update

--- src/context/AuthProvider/AuthProvider.jsx
+++ src/context/AuthProvider/AuthProvider.jsx
@@ -3,15 +3,19 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
 const AuthProvider = ({Children}) => {
     const user = null;
+    
+    const createUser = createUserWithEmailAndPassword(auth, email, password);
 
     const authInfo = {
         user,
+        createUser,
+
     };
 
     return (
